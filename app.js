b0eeca3fb07442805e668f3d1c761cfff0e70c55
nameofname <nameofemail@gmail.com>
2013-11-19 01:00:33 -0500

    Fixed bug in main app code. Now listening on correct port and application does not crash...

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 var express = require('express'),//require express - exposes the function express.
-    app = express(), //execute in 1 line
+    app = express(), //execute express...
     server = require('http').createServer(app), //create an http server using app as callback
     io = require('socket.io').listen(server),//create socket.io instance, listening on the http server.
     nerp = require('./testExport');
+
 
 // Set the templating engine to EJS:
 app.engine('html', require('ejs').renderFile);
@@ -14,30 +15,21 @@ app.use('/js', express.static(__dirname + '/src/js'));
 app.use('/css', express.static(__dirname + '/src/styles'));
 
 // Reference to socket.io library -- didn't want to move it into the src directory...
-app.use('/socket.io', express.static(__dirname + '/node_modules/socket.io/node_modules/socket.io-client/dist'));
+app.use('/socket_js', express.static(__dirname + '/node_modules/socket.io/node_modules/socket.io-client/dist'));
 
 // Set express to get html files out of the right place:
 app.use(express.static(__dirname + '/src/html'));
-
-// Listening on port 3K for now.
-app.listen(3000);
-
 
 app.get('/socket-test', function(req, res){
     res.sendfile(__dirname + '/src/html/socket-test.html');
 });
 
-/**
-* Set the io socket to do something on connection.
-*/
-console.log('socket?');
-io.sockets.on('connection', function (socket) {
-    console.log(socket);
+// Listening on port 3K for the web app.
+//app.listen(3000);
+
+//Listening on port 80 for the socket:
+server.listen(3000);
+
+io.on('connection', function(socket){
     socket.emit('news', { hello: 'world' });
-    socket.on('my other event', function (data) {
-        console.log(data);
-    });
 });
-
-console.log('BEW!');
-
--- src/js/pages/socket-test.js
+++ src/js/pages/socket-test.js
@@ -7,7 +7,7 @@
 (function(){
     "use strict";
 
-    var socket = io.connect('http://localhost:3000');
+    var socket = io.connect('http://localhost');
     socket.on('news', function (data) {
         console.log(data);
         socket.emit('my other event', { my: 'data' });
